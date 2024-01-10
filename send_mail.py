76d1ba1a458e19175d417c12c18f85c744babf4b
Emerson Bilieri <ebilieri@hotmail.com>
2024-01-10 20:57:22 -0300

    verificar se existe item na lista

--- send_mail.py
+++ send_mail.py
@@ -36,35 +36,36 @@ class EnviaEmail:
 
 
     def envia_email(self, lista_ativos: [], operacao: str) -> None:
-        senha = os.environ.get("senha_email")
-        email_from = os.environ.get("email")
-        now = datetime.today().isoformat()
+        if len(lista_ativos) > 0:
+            senha = os.environ.get("senha_email")
+            email_from = os.environ.get("email")
+            now = datetime.today().isoformat()
 
-        msg = EmailMessage()
-        msg['Subject'] = f"--> Alerta de {operacao} Python Ticker B3 <--"
-        msg['From'] = email_from
-        msg['To'] = os.environ.get("list_mail_to")
+            msg = EmailMessage()
+            msg['Subject'] = f"--> Alerta de {operacao} Python Ticker B3 <--"
+            msg['From'] = email_from
+            msg['To'] = os.environ.get("list_mail_to")
 
-        mensagem = ""
-        for row in lista_ativos:
-        
-            mensagem += f'''   {operacao} ---->>> {row["ticker"]} <<<-----\n'''
-                
-            mensagem += f'''   Valor Invest projetado -->> R$ {row["valor_projetado"]} <<-- \n'''
-                
-            mensagem += f'''   O ativo {row["ticker"]} chegou no preço de {operacao} -->> R$ {row["valor_atual"]} <<-- \n'''
+            mensagem = ""
+            for row in lista_ativos:
+            
+                mensagem += f'''   {operacao} ---->>> {row["ticker"]} <<<-----\n'''
+                    
+                mensagem += f'''   Valor Invest projetado -->> R$ {row["valor_projetado"]} <<-- \n'''
+                    
+                mensagem += f'''   O ativo {row["ticker"]} chegou no preço de {operacao} -->> R$ {row["valor_atual"]} <<-- \n'''
 
-            mensagem += f'''   Data/Hora: {now} \n'''
+                mensagem += f'''   Data/Hora: {now} \n'''
 
-            mensagem += f'''   -------------------------------------------------- \n'''
+                mensagem += f'''   -------------------------------------------------- \n'''
 
-            mensagem += '\n\n'
+                mensagem += '\n\n'
 
-        msg.set_content(f''' {mensagem} ''')
+            msg.set_content(f''' {mensagem} ''')
 
-        # Send mail
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        
-            smtp.login(email_from, senha)
-            smtp.send_message(msg)
+            # Send mail
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            
+                smtp.login(email_from, senha)
+                smtp.send_message(msg)
     
